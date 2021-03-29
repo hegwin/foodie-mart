8b6fc77aab2151e0bc6d229507bd6528603bbfe5
Hegwin <zwt315@163.com>
2021-03-29 13:04:47 +0800

    [#30] Add a basic view for meals in a restaurant

--- app/javascript/components/NavBar.jsx
+++ app/javascript/components/NavBar.jsx
@@ -2,20 +2,27 @@ import React, { Component } from 'react'
 
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/ToolBar'
+import Badge from '@material-ui/core/Badge'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import Typography from '@material-ui/core/Typography';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import Typography from '@material-ui/core/Typography'
 
 class NavBar extends Component {
   render() {
     return(
       <AppBar>
-        <ToolBar>
-          <IconButton>
+        <ToolBar style={{color: 'white'}}>
+          <IconButton color='inherit' edge='start'>
             <MenuIcon />
           </IconButton>
-          <Typography component='h1' variant='h6'>Hegwin's Foodie</Typography>
+          <Typography component='h1' variant='h6' style={{flexGrow: 1}}>Hegwin's Foodie</Typography>
+
+          <IconButton color='inherit' edge='end'>
+            <Badge badgeContent={3} color="secondary">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
         </ToolBar>
       </AppBar>
     )
--- app/javascript/pages/Restaurant/show.jsx
+++ app/javascript/pages/Restaurant/show.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
+import { withStyles } from '@material-ui/core/styles'
+
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography'
 
+import IconButton from '@material-ui/core/IconButton';
+import AddButton from '@material-ui/icons/AddCircle';
+import RemoveButton from '@material-ui/icons/RemoveCircle'
+
+
+
+const styles = theme => ({
+  restaurantHeader: {
+    height: 240,
+    marginBottom: '1em'
+    //  margin: '-1em -2em 0.5em -2em'
+  },
+  restaurantHeaderCover: {
+    height: 160
+  }
+})
 
 class Restaurant extends Component {
   constructor(props) {
@@ -46,15 +65,54 @@ class Restaurant extends Component {
   }
 
   render() {
-    const { name, description } = this.state
+    const { classes } = this.props
+    const { name, description, image_url, meals } = this.state
 
     return(
-      <Card>
-        {name} <br />
-        {description} <br />
-      </Card>
+      <div>
+        <Card className={classes.restaurantHeader}>
+          <CardMedia className={classes.restaurantHeaderCover} image={image_url} />
+          <CardContent>
+            <Typography component='h1' variant='h6'>{name}</Typography>
+            <Typography variant='body1'>{description}</Typography>
+          </CardContent>
+        </Card>
+        <Grid container spacing={1}>
+          {
+            meals.map(meal => {
+              return(
+                <Grid item key={meal.id} xs={12} md={6} lg={4}>
+                  <Card>
+                    <CardMedia style={{height: 100}} image={meal.image_url} />
+                    <CardContent>
+                      <Typography component='h2' variant='h6'>{meal.name}</Typography>
+                      <Typography variant='body1'>{meal.description}</Typography>
+                      <Typography variant='body1' color='primary' style={{marginTop: 16}}>{this.formatPrice(meal.price)}</Typography>
+                    </CardContent>
+                    <CardActions style={{justifyContent: 'flex-end', marginTop: -60}}>
+                      <IconButton>
+                        <RemoveButton color='primary' />
+                      </IconButton>
+                      <span>{1}</span>
+                      <IconButton>
+                        <AddButton color='primary' />
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+      </div>
     )
+  }
+
+  formatPrice(priceString) {
+    const price = Number(priceString);
+
+    return(new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(price))
   }
 }
 
-export default Restaurant
+export default withStyles(styles)(Restaurant)
